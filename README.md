badd2c785facc5732d847095c651ea23fb096def
Don Goodman-Wilson <don@slack-corp.com>
2018-02-28 10:08:49 +0100

    Now we can generate binaries.

---
+++ README.md
@@ -0,0 +1,3 @@
+# conan-gmp
+
+[![Build Status](https://travis-ci.org/DEGoodmanWilson/conan-libgcrypt.svg?branch=stable%2F1.7.3)](https://travis-ci.org/DEGoodmanWilson/conan-libgcrypt)
--- conanfile.py
+++ conanfile.py
@@ -1,16 +1,18 @@
-from conans import ConanFile
-import os, shutil
-from conans.tools import download, unzip, replace_in_file, check_md5
-from conans import CMake
-import json
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from conans import ConanFile, AutoToolsBuildEnvironment, tools
+import os
+
 
 class LibgcryptConan(ConanFile):
     name = "libgcrypt"
     version = "1.7.3"
-    branch = "master"
-    ZIP_FOLDER_NAME = "libgcrypt-%s" % version
-    generators = "cmake"
-    settings =  "os", "compiler", "arch", "build_type"
+    url = "http://github.com/DEGoodmanWilson/conan-libgcrypt"
+    description = "The GNU bignum library"
+    license = "https://www.gnupg.org/software/libgcrypt/index.html"
+    exports_sources = ["CMakeLists.txt"]
+    settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False],
                "enable_m_guard": [True, False],
                "disable_asm": [True, False],
@@ -22,98 +24,74 @@ class LibgcryptConan(ConanFile):
                "disable_aesni_support": [True, False],
                "disable_O_flag_munging": [True, False]}
                #TODO add in non-binary flags
-    requires = 'libgpg-error/1.24@DEGoodmanWilson/testing'
 
-    url = "http://github.com/DEGoodmanWilson/conan-libgcrypt"
     default_options = "shared=False", "enable_m_guard=False", "disable_asm=False", \
                       "enable_ld_version_script=False", "disable_endian_check=False", \
                       "enable_random_daemon=False", "disable_aesni_support=False", \
-		      "enable_hmac_binary_check=False", "disable_padlock_support=False", "disable_O_flag_munging=False"
+                      "enable_hmac_binary_check=False", "disable_padlock_support=False", \
+                      "disable_O_flag_munging=False"
+
+    requires = 'libgpg-error/1.24@DEGoodmanWilson/stable'
+
 
     def source(self):
-        zip_name = "libgcrypt-%s.tar.gz" % self.version
-        download("https://gnupg.org/ftp/gcrypt/libgcrypt/%s" % zip_name, zip_name)
-        check_md5(zip_name, "bb5b00cb70b1215833857fd690080fbb")
-        unzip(zip_name)
-        os.unlink(zip_name)
+        source_url = "https://gnupg.org/ftp/gcrypt/libgcrypt"
+        tools.get("{0}/libgcrypt-{1}.tar.gz".format(source_url, self.version))
+        extracted_dir = self.name + "-" + self.version
+        os.rename(extracted_dir, "sources")
 
-    def config(self):
-        del self.settings.compiler.libcxx
-
-    def generic_env_configure_vars(self, verbose=False):
-        """Reusable in any lib with configure!!"""
-
-        if self.settings.os == "Windows":
-            self.output.fatal("Cannot build on Windows, sorry!")
-            return
-
-        if self.settings.os == "Linux" or self.settings.os == "Macos":
-            libs = 'LIBS="%s"' % " ".join(["-l%s" % lib for lib in self.deps_cpp_info.libs])
-            ldflags = 'LDFLAGS="%s"' % " ".join(["-L%s" % lib for lib in self.deps_cpp_info.lib_paths]) 
-            archflag = "-m32" if self.settings.arch == "x86" else ""
-            cflags = 'CFLAGS="-fPIC %s %s %s"' % (archflag, " ".join(self.deps_cpp_info.cflags), " ".join(['-I"%s"' % lib for lib in self.deps_cpp_info.include_paths]))
-            cpp_flags = 'CPPFLAGS="%s %s %s"' % (archflag, " ".join(self.deps_cpp_info.cppflags), " ".join(['-I"%s"' % lib for lib in self.deps_cpp_info.include_paths]))
-            command = "env %s %s %s %s" % (libs, ldflags, cflags, cpp_flags)
-        elif self.settings.os == "Windows" and self.settings.compiler == "Visual Studio":
-            cl_args = " ".join(['/I"%s"' % lib for lib in self.deps_cpp_info.include_paths])
-            lib_paths= ";".join(['"%s"' % lib for lib in self.deps_cpp_info.lib_paths])
-            command = "SET LIB=%s;%%LIB%% && SET CL=%s" % (lib_paths, cl_args)
-            if verbose:
-                command += " && SET LINK=/VERBOSE"
-        
-        return command
-       
     def build(self):
-        if self.settings.os == "Windows":
-            self.output.fatal("Cannot build on Windows, sorry!")
-            return # no can do boss!
+        if self.settings.compiler == 'Visual Studio':
+            # self.build_vs()
+            self.output.fatal("No windows support yet. Sorry. Help a fellow out and contribute back?")
 
-        self.build_with_configure()
-            
-        
-    def build_with_configure(self):
-        config_options_string = ""
+        with tools.chdir("sources"):
+            env_build = AutoToolsBuildEnvironment(self)
+            env_build.fpic = True
 
-        for option_name in self.options.values.fields:
-            activated = getattr(self.options, option_name)
-            if activated:
-                self.output.info("Activated option! %s" % option_name)
-                config_options_string += " --%s" % option_name.replace("_", "-")
+            config_args = []
+            for option_name in self.options.values.fields:
+                if(option_name == "shared"):
+                    if(getattr(self.options, "shared")):
+                        config_args.append("--enable-shared")
+                        config_args.append("--disable-static")
+                    else:
+                        config_args.append("--enable-static")
+                        config_args.append("--disable-shared")
+                else:
+                    activated = getattr(self.options, option_name)
+                    if activated:
+                        self.output.info("Activated option! {0}".format(option_name))
+                        config_args.append("--{0}".format(option_name))
 
-        # find the libgpg-error folder, so we can set the binary path for configuring it
-        gpg_error_path = ""
-        for path in self.deps_cpp_info.lib_paths:
-            if "libgpg-error" in path:
-                gpg_error_path = '/lib'.join(path.split("/lib")[0:-1]) #remove the final /lib. There are probably better ways to do this.
-                break
+            # find the libgpg-error folder, so we can set the binary path for configuring it
+            gpg_error_path = ""
+            for path in self.deps_cpp_info.lib_paths:
+                if "libgpg-error" in path:
+                    gpg_error_path = '/lib'.join(path.split("/lib")[0:-1]) #remove the final /lib. There are probably better ways to do this.
+                    config_args.append("--with-libgpg-error-prefix={0}".format(gpg_error_path))
+                    break
 
-        configure_command = "cd %s && %s ./configure --prefix=%s --enable-static --enable-shared --with-libgpg-error-prefix=%s %s" % (self.ZIP_FOLDER_NAME, self.generic_env_configure_vars(), self.package_folder, gpg_error_path, config_options_string)
-        self.output.warn(configure_command)
-        self.run(configure_command)
-        self.run("cd %s && make" % self.ZIP_FOLDER_NAME)
+            print config_args
+
+            env_build.configure(args=config_args)
+            env_build.make()
        
-
     def package(self):
-        if self.settings.os == "Windows":
-            self.output.fatal("Cannot build on Windows, sorry!")
-            return
-
-        self.copy("*.h", dst="include", src="%s/src" % (self.ZIP_FOLDER_NAME), keep_path=True)
-        if self.options.shared:
-            self.copy(pattern="*.so*", dst="lib", src=self.ZIP_FOLDER_NAME, keep_path=False)
-            self.copy(pattern="*.dll*", dst="bin", src=self.ZIP_FOLDER_NAME, keep_path=False)
-        else:
-            self.copy(pattern="*.a", dst="lib", src="%s" % self.ZIP_FOLDER_NAME, keep_path=False)
+        self.copy(pattern="COPYING*", src="sources")
+        self.copy("*.h", dst="include", src="sources/src", keep_path=True)
+        # self.copy(pattern="*.dll", dst="bin", src="bin", keep_path=False)
+        self.copy(pattern="*.lib", dst="lib", src="sources", keep_path=False)
+        self.copy(pattern="*.a", dst="lib", src="sources", keep_path=False)
+        self.copy(pattern="*.so*", dst="lib", src="sources", keep_path=False)
+        self.copy(pattern="*.dylib", dst="lib", src="sources", keep_path=False)
         
-        self.copy(pattern="*.lib", dst="lib", src="%s" % self.ZIP_FOLDER_NAME, keep_path=False)
-        
-        self.copy("libgcrypt-config", dst="bin", src="%s/src" % self.ZIP_FOLDER_NAME, keep_path=False)
-        self.copy("hmac256", dst="bin", src="%s/src" % self.ZIP_FOLDER_NAME, keep_path=False)
-        self.copy("dumpsexp", dst="bin", src="%s/src" % self.ZIP_FOLDER_NAME, keep_path=False)
-        self.copy("mpicalc", dst="bin", src="%s/src" % self.ZIP_FOLDER_NAME, keep_path=False)
-
+        self.copy("libgcrypt-config", dst="bin", src="sources/src", keep_path=False)
+        self.copy("hmac256", dst="bin", src="sources/src", keep_path=False)
+        self.copy("dumpsexp", dst="bin", src="sources/src", keep_path=False)
+        self.copy("mpicalc", dst="bin", src="sources/src", keep_path=False)
         
     def package_info(self):
-        self.cpp_info.libs = ['gcrypt']
+        self.cpp_info.libs = tools.collect_libs(self)
 
 
